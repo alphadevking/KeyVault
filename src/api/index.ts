5a501245c66e7ee98653251f6d71574a3a5ede4b
alphadevking <98643372+alphadevking@users.noreply.github.com>
2025-01-25 00:32:31 +0100

    change env variable name

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 const serverUrl = import.meta.env.VITE_SERVER_APP_URL;
-const serverApiKey = import.meta.env.VITE_SERVER_API_KEY;
+const serverApiKey = import.meta.env.VITE_SERVER_API;
 
 export { default as viewDB } from "./viewDB";
 export { default as cloudGenerate, type GenerateProps } from "./generate";
